aabec693f2cc98a6030360dd3cf3b066fec7777e
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-06-10 14:38:37 +0200

    Created the essential functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// The player factory
+const Player = function (marker) {
+  return {};
+};
+
+const gameBoard = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  return {};
+})();
+
+// The UI functionality is on this module
+const displayController = (function () {
+  return {};
+})();
+
+// The logic of the game is on this module
+const game = (function () {
+  return {};
+})();
