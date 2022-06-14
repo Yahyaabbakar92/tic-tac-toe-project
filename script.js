98a39a24d69f6f556aa9a94329202de2c30e75c8
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-06-14 11:26:12 +0200

    Added a function to render board content to webpage

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // The player factory
 const Player = function (marker) {
-  return {};
+  let getMarker = () => marker;
+
+  return { getMarker };
 };
 
 const gameBoard = (function () {
-  let board = ["", "", "", "", "", "", "", "", ""];
-  return {};
+  let board = ["X", "O", "", "", "", "", "X", "", "o"];
+
+  function getBoardValues() {
+    // board.forEach((item) => console.log(item[0]));
+    // console.log(board[1]);
+    return board[8];
+  }
+  return { getBoardValues };
 })();
 
 // The UI functionality is on this module
 const displayController = (function () {
+  let cells = document.querySelectorAll("[data-cell]");
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      e.target.textContent = gameBoard.getBoardValues();
+      // console.log(e.target.textContent);
+    });
+  });
+
   return {};
 })();
 
